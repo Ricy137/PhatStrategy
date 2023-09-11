29c09b4fee7bf454d2bb79f94d514978b5115709
Ricy <1932353948@qq.com>
2023-09-12 00:38:52 +0800

    fix: updated mainnet address

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -5,7 +5,7 @@ export const CHAIN_ID_HEX = "0x13881";
 export const SCAN_URL = "https://api-testnet.polygonscan.com/";
 
 // Mainnet configuration
-// export const CONTRACT_ADDRESS = "0xa8b024F2622D89456018A3F5A8bc834b9FB8215E";
+// export const CONTRACT_ADDRESS = "0xA6d74171bD5592ba4295F299887222fd996Dc16E";
 // export const CHAIN_ID = "137";
 // export const CHAIN_ID_HEX = "0x89";
 // export const SCAN_URL = "https://api.polygonscan.com/";
