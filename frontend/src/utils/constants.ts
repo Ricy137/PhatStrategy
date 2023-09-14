12f4b18e2fa5a5abe33f733ea50120b7aee38bf7
Ricy <1932353948@qq.com>
2023-09-14 23:15:14 +0800

    feat: initialize mainnet

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,11 +1,11 @@
 //Testnet configuration
-export const CONTRACT_ADDRESS = "0x72EfA99059F29e87340d153aB12CCc0E562985e9";
-export const CHAIN_ID = "80001";
-export const CHAIN_ID_HEX = "0x13881";
-export const SCAN_URL = "https://api-testnet.polygonscan.com/";
+// export const CONTRACT_ADDRESS = "0x72EfA99059F29e87340d153aB12CCc0E562985e9";
+// export const CHAIN_ID = "80001";
+// export const CHAIN_ID_HEX = "0x13881";
+// export const SCAN_URL = "https://api-testnet.polygonscan.com/";
 
 // Mainnet configuration
-// export const CONTRACT_ADDRESS = "0x682b19588Fa42040ab63D0B3Fd33F6b631EA8e15";
-// export const CHAIN_ID = "137";
-// export const CHAIN_ID_HEX = "0x89";
-// export const SCAN_URL = "https://api.polygonscan.com/";
+export const CONTRACT_ADDRESS = "0x682b19588Fa42040ab63D0B3Fd33F6b631EA8e15";
+export const CHAIN_ID = "137";
+export const CHAIN_ID_HEX = "0x89";
+export const SCAN_URL = "https://api.polygonscan.com/";
